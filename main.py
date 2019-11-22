84967d00085d0cef20a401ee7f0b6f4344c9342e
Spraynard <kellan.martin@gmail.com>
2019-11-21 20:13:28 -0500

    Updating main.py
    Comments and such

--- main.py
+++ main.py
@@ -31,10 +31,19 @@ Hostname: {hostname}
 class ProxyBoyBase(http.server.BaseHTTPRequestHandler):
     def __init__(self, LoggerClass, request, client_address, server):
         self.logger = LoggerClass
-        self._headers_buffer = []
+        self._headers_buffer = [] # Needed to not error out. Did I accidentally delete the headers buffer?
         super(ProxyBoyBase, self).__init__(request, client_address, server)
 
-    def assign_port(self, parsed_url):
+    def do_PROXY(self):
+        """
+        Main function. Called on every request.
+        Must be overriden.
+        """
+        pass
+
+    def get_a_port(self, parsed_url):
+        """Returns a port that is either the default, or a port given in a urlparse() string.
+        """
         port = parsed_url.port
 
         # Search through to find a port string
@@ -47,9 +56,12 @@ class ProxyBoyBase(http.server.BaseHTTPRequestHandler):
         return port
 
     def get_client_url(self, client_host):
+        """Builds and sanitizes the client url string so that we can use it
+        in subsequent requests.
+        """
         ssl = False
         parsed_url  = urllib.parse.urlparse(client_host, 'http')
-        port = self.assign_port(parsed_url)
+        port = self.get_a_port(parsed_url)
 
         if port == '443':
             ssl = True
@@ -86,10 +98,11 @@ class ProxyBoyBase(http.server.BaseHTTPRequestHandler):
             if not self.parse_request():
                 # An error code has been sent, just exit
                 return
-            # Do a proxy. With pyroxy.
-            mname = 'do_PROXY'
-            method = getattr(self, mname)
-            method()
+            # Do a proxy. With ProxyBoy.
+            if not getattr(self, 'do_PROXY'):
+                self.send_error(HTTPStatus.INTERNAL_SERVER_ERROR)
+                return
+            self.do_PROXY()
             self.wfile.flush() #actually send the response if not already done.
         except socket.timeout as e:
             #a read or a write timed out.  Discard this connection
@@ -111,15 +124,21 @@ class ProxyBoy(ProxyBoyBase):
         opener.addheaders = self.headers.items() + [
             ("X-Forwarded-Host", self.headers.get('Host'))
         ]
-        self.end_headers()
 
+        # Output headers in output buffer and close buffer,
+        # as we do not care a single bit about the headers at this point.
+        self.end_headers()
         self.wfile.close()
+
+        # Create a new output buffer to put response information on.
         self.wfile = self.connection.makefile('wb', self.wbufsize)
 
         try:
             print(client_url)
             response = opener.open(client_url)
             self.headers = response.getheaders()
+
+            # If response is gzipped then we have to unzip it.
             encoding = response.getheader('Content-Encoding')
 
             if encoding == 'gzip':
@@ -128,17 +147,11 @@ class ProxyBoy(ProxyBoyBase):
         except HTTPError as e:
             print('The server couldn\'t fulfill the request.')
             print(f"Error code: {e.code}\nError message: {e.reason}")
-            return
         except URLError as e:
             print('We failed to reach a server.')
             print('Reason: ', e.reason)
-            return
         else:
-       #     # All good, set up the headers for our response
-       #     headers = response.info().items()
-       #
-       #     for header in self.headers:
-       #         self.send_header(header[0], header[1])
+            # Output the request response to the user.
             self.wfile.write(response.read())
         return
 # Entry point for starting up a server.
